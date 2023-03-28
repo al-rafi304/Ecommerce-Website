2a3bdfead8421b941bf59af60b1162cc23392bf1
Mohammad Al Rafi <arafi304@gmail.com>
2023-03-28 17:22:39 +0600

    User Authentication Done
    Login, logout, added bootstrap

--- ecom_website/member/views.py
+++ ecom_website/member/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
+def login_member(request):
+    # If form has been submitted
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        # Catching erros
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            # Login error message
+            messages.success(request, ('Error loging in !!!'))
+            return redirect('login')
+    
+    # Landing page
+    else:
+        return render(request, 'member/login.html', {})
+    
+def logout_member(request):
+    logout(request)
+    messages.success(request, 'You were logged out')
+    return redirect('home')
--- ecom_website/store/views.py
+++ ecom_website/store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def home(request):
+    products = Product.objects.all()
+    return render(
+        request, 'store/home.html',
+        {
+            'Products': products,
+            'request': request,
+        }
+        )
