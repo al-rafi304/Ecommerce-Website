c7a66d7e572b61a859a06afa5eddd8f394667e0a
Mohammad Al Rafi <arafi304@gmail.com>
2023-03-31 20:02:50 +0600

    Changed media directory and deleted the previous one

--- ecom_website/ecom_website/settings.py
+++ ecom_website/ecom_website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
